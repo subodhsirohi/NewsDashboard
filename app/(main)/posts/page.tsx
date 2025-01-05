193fcfdd510d5d0917ac7a770ff73d713f1cabc7
SUBODH SIROHI <subodhsirohiair@gmail.com>
2025-01-05 17:28:11 +0530

    first commit

---
+++ app/(main)/posts/page.tsx
@@ -0,0 +1,15 @@
+import PostsTable from '@/components/posts/PostTable';
+import BackButton from '@/components/BackButton';
+import PostsPagination from '@/components/posts/PostPagination';
+
+const PostsPage = () => {
+  return (
+    <>
+      <BackButton text='Go Back' link='/' />
+      <PostsTable />
+      <PostsPagination />
+    </>
+  );
+};
+
+export default PostsPage;
